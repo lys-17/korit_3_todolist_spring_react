4c12b3762b42e6df3dbbf879837090a2c943afb9
lys-17 <lkl10055@naver.com>
2025-06-10 16:20:50 +0900

    20250610

--- advanced-todo-app/src/App.tsx
+++ advanced-todo-app/src/App.tsx
@@ -1,19 +1,21 @@
 import './App.css'
-import { useState, useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import type { Todo } from './types/Todo';
 import TodoForm from "./components/TodoForm"
 import {TodoList} from "./components/TodoList"
-import { addTodoApi, deleteTodoApi, getAllTodos, toggleTodoApi } from './services/todoService';
+import { getAllTodos, addTodoApi, toggleTodoApi, deleteTodoApi } from './services/todoService';
 import axios from 'axios';
-import { GoogleLogin, type CredentialResponse } from '@react-oauth/google';
+import { GoogleLogin, type CredentialResponse} from '@react-oauth/google';
 
 function App() {
+
   const [ todos, setTodos ] = useState<Todo[]>([]);
-    const [isLoading, setIsLoading] = useState<boolean>(false);
-    const [authToken, setAuthToken] = useState<string | null>(() => localStorage.getItem('authToken'));
+  const [ isLoading, setIsLoading ] = useState<boolean>(false);
+  const [ authToken, setAuthToken ] = useState<string | null>(() => localStorage.getItem('authToken'));
 
   const handleLoginSuccess = (credentialResponse: CredentialResponse) => {
     const idToken = credentialResponse.credential;
+
     if (idToken) {
       setAuthToken(idToken);
       localStorage.setItem('authToken', idToken);
@@ -21,72 +23,72 @@ function App() {
   }
 
   const handleLoginError = () => {
-    console.log('로그인에 실패하였습니다');
+    console.log('로그인 실패')
   }
 
   const handleLogout = () => {
     setAuthToken(null);
     localStorage.removeItem('authToken');
     setTodos([]);
-  };
+  }
 
-
+  // 최초 랜더링 시 useEffect
   useEffect(() => {
-    const fetchTodosFromServer = async () : Promise<void> => {
+    const fetchTodosFromServer = async (): Promise<void> => {
       if (authToken) {
         try {
           setIsLoading(true);
           const serverTodos = await getAllTodos();
           setTodos(serverTodos);
-        }
-        catch (error) {
-          console.log('서버에서 데이터를 가지고 오는 데 실패함 : ', error);
-          if (axios.isAxiosError(Error)&& (error.response?.status === 401 || error.response?.status === 403)) {
+        } catch (error) {
+          console.log('서버에서 데이터를 가져오는 데 실패했습니다: ', error);
+          if (axios.isAxiosError(error) && (error.response?.status === 401 || error.response?.status === 403)) {
             handleLogout();
           }
-        }
-        finally {
+        } finally {
           setIsLoading(false);
         }
       }
-    };
+    }
     fetchTodosFromServer();
-  },[authToken]);
+  }, [authToken]);
 
   const handleAddTodo = async (text: string): Promise<void> => {
-    if(authToken)return;
+    if (!authToken) return;
     try {
       setIsLoading(true);
       const newTodo = await addTodoApi(text);
       setTodos(prevTodos => [...prevTodos, newTodo]);
+      getAllTodos();
       setIsLoading(false);
     } catch (error) {
-      console.log('todo를 추가하는 데 실패했습니다 : ', error);
+      console.log(`todo를 추가하는 데 실패했습니다 : `, error);
     }
   }
 
-  const handleToggleComplete = async (id:number): Promise<void> => {
+  const handleToggleComplete = async (id: number): Promise<void> => {
+    if (!authToken) return;
     try {
       const todoToToggle = todos.find(todo => todo.id === id);
       if (!todoToToggle) return;
-      const upatedTodo = await toggleTodoApi(id, todoToToggle.completed);
-      setTodos(prevTodos =>
-        prevTodos.map(todo => (todo.id === id ? upatedTodo : todo))
+      const updateTodo = await toggleTodoApi(id, todoToToggle.completed);
+      setTodos(prevTodos => 
+        prevTodos.map(todo => (todo.id === id ? updateTodo : todo))
       );
     } catch (error) {
-      console.log('완료 상태 변경에 실패했습니다 : ', error);
+      console.log("완료 상태 변경에 실패했습니다 : ", error);
     }
-  };
+  }
 
-  const handleDeleteTodo = async (id: number) : Promise<void> => {
+  const handleDeleteTodo = async (id: number): Promise<void> => {
+    if (!authToken) return;
     try {
       await deleteTodoApi(id);
       setTodos(prevTodos => prevTodos.filter(todo => todo.id !== id));
     } catch (error) {
-      console.log('todo를 지우는 데 실패했습니다 :', error);
+      console.log('todo를 지우는데 실패했습니다 : ', error);
     }
-  } 
-
+  }
 
   return (
     <div>
@@ -94,31 +96,31 @@ function App() {
         <h1>Todo List</h1>
         <div>
           {
-            authToken ? () : (
+            authToken ? (
               <button onClick={handleLogout}>Logout</button>
             ) : (
-              <GoogleLogin onSuccess={handleLoginSuccess} onError = {handleLoginError}/>
+              <GoogleLogin onSuccess={handleLoginSuccess} onError={handleLoginError}>
+
+              </GoogleLogin>
             )
           }
         </div>
       </header>
       <main>
-          {
-            authToken ? ( 
-              isLoading ? (
+        {
+          authToken ? (
+            isLoading ? (
               <p>목록을 불러오는 중입니다...</p>
             ) : (
               <>
-              <TodoForm onAddTodo={handleAddTodo}/>
-              <TodoList todos={todos} 
-              onToggleComplete={handleToggleComplete} 
-              onDeleteTodo={handleDeleteTodo}/>
+                <TodoForm onAddTodo={handleAddTodo}/>
+                <TodoList todos={todos} onToggleComplete={handleToggleComplete} onDeleteTodo={handleDeleteTodo}/>
               </>
             )
           ) : (
-              <h2>로그인하여 To do List를 작성해보세요</h2>
-            )
-          }
+            <h2>로그인 후에 Todo List를 작성하시오</h2>
+          )
+        }
       </main>
     </div>
   )
--- advanced-todo-app/src/services/todoService.ts
+++ advanced-todo-app/src/services/todoService.ts
@@ -4,29 +4,30 @@ import apiClient from "./apiClient";
 export const getAllTodos = async (): Promise<Todo[]> => {
   try {
     const response = await apiClient.get<Todo[]>('/todos');
+    return Array.isArray(response.data) ? response.data : [];
+  } catch (error) {
+    console.log('Error fetching todos', error);
+    return [];
+  }
+}
+
+export const addTodoApi = async (text: string): Promise<Todo> => {
+  try {
+    const response = await apiClient.post('/todos', {text, complete: false});
     return response.data;
+    
+  } catch (error) {
+    console.log("Error addTodo: ", error);
+    throw error;
   }
-  catch (error) {
-    console.log('Error fetching todos :'. error);
-  }
-};
+}
 
-export const addTodoApi = async (text:string): Promise<Todo> => {
-  try{
-      const response = await apiClient.post('/todos', {text, completed: false});
-      return response.data;
-    }
-    catch (error) {
-      console.log("Error adding todo: ", error);
-    }
-  };
-
-export const toggleTodoApi =async (id: number, completed:boolean): Promise<Todo> => {
+export const toggleTodoApi = async (id: number, completed: boolean): Promise<Todo> => {
   try {
     const response = await apiClient.patch<Todo>(`/todos/${id}`, {completed: !completed});
     return response.data;
   } catch (error) {
-    console.log(`Error toggling todo ${id} : `, error);
+    console.log(`Error toggleTodo: ${id}`, error);
     throw error;
   }
 }
@@ -35,7 +36,7 @@ export const deleteTodoApi = async (id: number): Promise<void> => {
   try {
     await apiClient.delete(`/todos/${id}`);
   } catch (error) {
-    console.log(`Error deleting todo ${id} : `, error);
+    console.log(`Error delete: ${id}`, error);
     throw error;
   }
 }
